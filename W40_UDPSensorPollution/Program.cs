2b3764f17a9d768dcfac033b5bef00f581a3f23c
Lardman <Lardman@DESKTOP-FQBNP2K>
2017-10-06 12:48:24 +0200

    Add project files.

---
+++ W40_UDPSensorPollution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W40_UDPSensorPollution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
